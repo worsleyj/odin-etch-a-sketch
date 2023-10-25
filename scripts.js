c82264fd544fd589beb68be1aeb653da6a07c4df
worsleyj <josephworsley23@gmail.com>
2023-10-25 13:38:07 -0400

    added generate function and reset function for grid manipulation

--- scripts.js
+++ scripts.js
@@ -1,11 +1,44 @@
-const DIV_NUM = 16;
+let squares = 16; // how many pixels to add to grid, max of 100
+const gridButton = document.querySelector("#grid-button");
 const container = document.querySelector("#container");
 
-for(i = 0; i < DIV_NUM; i++) {
-    let square = document.createElement("div");
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "green";
-    })
-    square.className = "square";
-    container.appendChild(square);
+function generate() {
+    for(i = 0; i < squares; i++) {
+        let column = document.createElement("div");
+        column.className = "column";
+        container.appendChild(column);
+        for(j = 0; j < squares; j++) {
+            let square = document.createElement("div");
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = "green";
+            })
+            square.addEventListener("click", () => {
+                square.style.backgroundColor = "white";
+            })
+            square.className = "square";
+            column.appendChild(square);
+        }
+    }
+    
 }
+
+function resetGrid() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+}
+
+gridButton.addEventListener("click", () => {
+    squares = prompt("What size (x by x) should the grid be? Must be below 100.");
+    if(squares <= 100) {
+        resetGrid();
+        generate();
+    } else {
+        square = 16;
+        resetGrid();
+        generate();
+        alert("Size too big! Must be below 100 squares.")
+    }
+})
+
+generate()
