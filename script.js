32110d5e263a9dbfffd160ef6ce2ff17e95ddb01
worsleyj <josephworsley23@gmail.com>
2024-07-15 16:05:32 -0400

    add toggle for square behavior

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const squareContainer = document.querySelector(".container");
 const newGridBtn = document.querySelector(".new-grid");
+const hoverToggle = document.querySelector(".hover-toggle");
+const clickToggle = document.querySelector(".click-toggle");
+
+let gridSize = 16;
+let inputType = "mouseover";
+
+hoverToggle.addEventListener("click", () => {
+    inputType = "mouseover";
+    hoverToggle.style.background = "white";
+    clickToggle.style.background = "grey";
+    generateSquares(gridSize);
+})
+
+clickToggle.addEventListener("click", () => {
+    inputType = "click";
+    clickToggle.style.background = "white";
+    hoverToggle.style.background = "grey";
+    generateSquares(gridSize);
+})
 
 newGridBtn.addEventListener("click", () => {
     selectSize();
 })
 
 function selectSize() {
-    let gridSize = prompt("What should the size of the grid be? (1-100)", 16)
+    gridSize = prompt("What should the size of the grid be? (1-100)", 16)
     if (gridSize <= 100 && gridSize > 0) {
         generateSquares(gridSize);
     } else {
@@ -15,21 +34,28 @@ function selectSize() {
     }
 }
 
+function clearGrid() {
+    while (squareContainer.firstChild) {
+        squareContainer.removeChild(squareContainer.lastChild);
+    }
+}
+
 function generateSquares(gridSize) {
+    clearGrid();
     let sizeSquared = gridSize ** 2;
 
     for (let i = 0; i < sizeSquared; i++) {
         const div = document.createElement("div");
-        div.textContent = "test";
         div.style.width = 100/gridSize + "%";
         div.style.height = "50px";
 
-        div.addEventListener("mouseover", () => {
-            div.style.background = "black";
+        div.addEventListener(inputType, () => {
+            // div.style.background = "black";
+            div.style.background = "rgb(0 0 0 / 40%)";
         })
 
         squareContainer.appendChild(div);
     }
 }
 
-generateSquares(16)
+generateSquares(gridSize);
